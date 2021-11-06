ff2b312a8997b4edd6a9e04e4b0d2bb9e3ca1416
mrbushik <mrbushik1@gmail.com>
2021-11-06 22:32:29 +0300

    added product-slider and  product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function(){
-
-
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+    $(".product-slider__inner").slick({
+       dots: true,
+       arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 2
+    });
+    var mixer = mixitup('.products__inner-box');
     
 });
